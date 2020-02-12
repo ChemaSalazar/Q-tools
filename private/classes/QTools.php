30c580c4dc4633c9da6f044d773198c6ede5425a
ChemaSalazar <ChemaSalazar@Outlook.com>
2020-02-11 22:00:16 -0600

    gitignore upload

--- private/classes/QTools.php
+++ private/classes/QTools.php
@@ -6,11 +6,16 @@
  * Time: 5:59 PM
  */
 
+include('../config/db.php');
+
 class QTools
 {
-    public static function start_database(){
+    public static function start_database($dsn, $user, $pass, $options){
         try{
             $db_connect = new PDO($dsn,$user,$pass,$options);
+        }
+        catch (\PDOException $e){
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
         }
     }
 
